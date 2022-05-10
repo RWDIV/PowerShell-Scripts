e2ee41a6693523fee7d6ff06d946fa5cb2a544cd
RWDIV <104102195+RWDIV@users.noreply.github.com>
2022-05-10 16:39:59 -0400

    Update Multiple Computers.ps1
    Added 2nd Script

--- Multiple Computers.ps1
+++ Multiple Computers.ps1
@@ -3,3 +3,11 @@
 Get-ADComputer -Filter 'enabled -eq "true"' -Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
 Sort-Object -Property Operatingsystem |
 Select-Object -Property Name,Operatingsystem,OperatingSystemVersion
+
+
+# Create list of computers that have not logged onto the network within 30 days
+
+$Days = 30
+$Time = (Get-Date).Adddays(-($Days))
+Get-ADComputer -Filter {LastLogonTimeStamp -lt $Time} -Properties * | Select Name, LastLogonDate
+
