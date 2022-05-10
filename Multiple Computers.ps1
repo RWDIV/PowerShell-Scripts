0305131243b21f217426c1148d9094ab667e5eac
RWDIV <104102195+RWDIV@users.noreply.github.com>
2022-05-10 16:36:56 -0400

    Update and rename Multiple Computers to Multiple Computers.ps1
    Added PowerShell Script

---
+++ Multiple Computers.ps1
@@ -0,0 +1,5 @@
+Create list of computers with a particular operating system installed
+
+Get-ADComputer -Filter 'enabled -eq "true"' -Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
+Sort-Object -Property Operatingsystem |
+Select-Object -Property Name,Operatingsystem,OperatingSystemVersion
