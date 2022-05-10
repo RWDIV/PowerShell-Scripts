dc87d71256f74e2de96f80e6a8df8e722523ff61
RWDIV <104102195+RWDIV@users.noreply.github.com>
2022-05-10 16:38:17 -0400

    Update Multiple Computers.ps1
    Added Header

--- Multiple Computers.ps1
+++ Multiple Computers.ps1
@@ -1,4 +1,4 @@
-Create list of computers with a particular operating system installed
+# Create list of computers with a particular operating system installed
 
 Get-ADComputer -Filter 'enabled -eq "true"' -Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
 Sort-Object -Property Operatingsystem |
