ced5d24b189112ccf94864dd64fecce203863217
RWDIV <104102195+RWDIV@users.noreply.github.com>
2022-05-11 15:49:39 -0400

    Update Remote.ps1
    created function

--- Remote.ps1
+++ Remote.ps1
@@ -1,3 +1,7 @@
-# Create script to remote restart computer
+# Created script to remote restart computer
 
-Restart-Computer -ComputerName LON-DC1 -Wait -For PowerShell -Timeout 300 -Delay 
+function Remote-Restart
+{
+$ComputerName = Read-Host "Please enter the computer name."
+Restart-Computer -ComputerName $ComputerName -Wait -For PowerShell -Timeout 300 -Delay 2 
+}
