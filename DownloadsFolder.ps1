6806fcb505c8c6cd7a1fe0f8d58e70fa99d86ff8
RWDIV <104102195+RWDIV@users.noreply.github.com>
2022-05-12 10:45:44 -0400

    Update DownloadsFolder.ps1
    Adjusted -lt

--- DownloadsFolder.ps1
+++ DownloadsFolder.ps1
@@ -1,3 +1,3 @@
 # Automatically remove items from Downloads folders 60+ days old
 
-Get-ChildItem -Path “C:\Users\Administrator.ADATUM\Downloads” -Recurse | Where-Object {($_.LastWriteTime – lt (Get-Date).AddDays(-60))} | Remove-Item
+Get-ChildItem -Path “C:\Users\Administrator.ADATUM\Downloads” -Recurse | Where-Object {($_.LastWriteTime –lt (Get-Date).AddDays(-60))} | Remove-Item
