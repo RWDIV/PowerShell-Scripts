53abf290306ed912d0c4737ea6d93077bd7fb499
RWDIV <104102195+RWDIV@users.noreply.github.com>
2022-05-12 09:31:42 -0400

    Update Remote.ps1
    Added instructions

--- Remote.ps1
+++ Remote.ps1
@@ -1,4 +1,19 @@
 # Created script to remote restart computer
+# Created a function called "Remote-Restart" to save time having to type out the same code over and over.
+function Remote-Restart
+{
+# Created a variable "$CName" with user input by having the user type out the name of the computer that needs restarted.
+$CName = Read-Host "Please enter the computer name."
+# Restart-Computer is the cmdlet that restarts the local or remote computer
+# -ComputerName is a parameter that can accept an array of computer names
+# $CName is the variable that's inputted by the user from the previous line of code. 
+# -Wait parameter waits indefinitely for the computer(s) to restart. 
+# -For and -Delay parameters wait for particular services to become available on the restarted computers.
+# -Delay: Specifies the frequency of queries, in seconds.
+Restart-Computer -ComputerName $CName -Wait -For PowerShell -Delay 2 
+}
+
+
 
 function Remote-Restart
 {
