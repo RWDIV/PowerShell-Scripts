c30e57d1a82060ef0bc5684a5744a9c7cbc35797
RWDIV <104102195+RWDIV@users.noreply.github.com>
2022-05-12 10:42:39 -0400

    Update Multiple Computers.ps1
    Updated Instructions

--- Multiple Computers.ps1
+++ Multiple Computers.ps1
@@ -1,13 +1,31 @@
 # Create list of computers with a particular operating system installed
 
-Get-ADComputer -Filter 'enabled -eq "true"' -Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
+# Get-ADComputer is the cmdlet retrieves computer(s)
+# -Filter * is the parameter that retrieves all computers
+# -Properties retrieves the three properties listed below only 
+Get-ADComputer -Filter * -Properties Name,Operatingsystem,OperatingSystemVersion |
+
+# Sort-Object is the cmdlet that will sort Operatingsystem property by ascending order
+Sort-Object -Property Operatingsystem |
+
+# Select-Object is the cmdlet that outputs the three properties listed below
+Select-Object -Property Name,Operatingsystem,OperatingSystemVersion
+
+
+
+Get-ADComputer -Filter * -Properties Name,Operatingsystem,OperatingSystemVersion |
 Sort-Object -Property Operatingsystem |
 Select-Object -Property Name,Operatingsystem,OperatingSystemVersion
+
+
+############################################
+
+
 
 
 # Create list of computers that have not logged onto the network within 30 days
 
 $Days = 30
-$Time = (Get-Date).Adddays(-($Days))
-Get-ADComputer -Filter {LastLogonTimeStamp -lt $Time} -Properties * | Select Name, LastLogonDate
+$Time = (Get-Date).Adddays(-(-$Days))
+Get-ADComputer -Filter {LastLogonTimeStamp -gt $Time} -Properties * | Select Name, LastLogonDate
 
